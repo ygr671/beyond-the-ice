cd7ca6cf392c400da1cf0e4f129de093d977c370
raphael.roche <raphael.roche@etu.unistra.fr>
2025-09-19 14:43:18 +0200

    ajout d'un tutoriel

--- menu_principal/menu_principal.gd
+++ menu_principal/menu_principal.gd
@@ -1,11 +1,19 @@
 extends Control
 @onready var gamePlay = preload("res://main_scene.tscn")
+@onready var tuto = preload("res://tuto//scene1.tscn")
 
 
 
 func _on_start_btn_button_down() -> void:
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_packed(gamePlay)
 
 
 func _on_quit_btn_button_down() -> void:
 	get_tree().quit()
+
+
+func _on_tutorial_button_down() -> void:
+	await get_tree().create_timer(1).timeout
+	get_tree().change_scene_to_packed(tuto)
+	
