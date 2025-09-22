6dd672c32d88930252bdff3e1a6ff80be56b5d0a
Muhammed ERDAL <muhammed.erdal@unistra.fr>
2025-09-22 22:04:13 +0200

    Backend du site web terminé, il faudrait ajouter encore quelques conditions pour la sécurité. Front-end manquant.

--- site-web/api/handlePlayer.php
+++ site-web/api/handlePlayer.php
@@ -2,12 +2,13 @@
   // Check request method
   if ($_SERVER["REQUEST_METHOD"] != "POST")
   {
-    header("Location: error.html");
+    http_response_code(405); // Method not allowed response code
+    echo json_encode(["error" => "Invalid method"]);
     exit;
   }
 
   // Including bdd.php
-  require_once("bdd.php");
+  require_once("../bdd.php");
 
   // Response type
   header('Content-Type: application/json');
@@ -28,16 +29,48 @@
   $last_name = $data["last_name"];
   $score = (int)$data["score"];
 
-  
-
-  /*
-  // Login and password emptiness check
-  if (empty($_POST["ndf"]) || empty($_POST["score"]))
+  // Check if types are both string and integer and is they're not empty
+  if (
+    !isset($data["last_name"], $data["score"]) ||
+    empty($data["last_name"]) ||
+    !is_string($data["last_name"]) ||
+    !is_int($data["score"]))
   {
-      exit;
+    http_response_code(400);
+    echo json_encode(["error" => "Invalid Data"]);
+    exit;
   }
-  */
+
+
+  // check if received last name isn't already in the database
+  $present = false;
+  $check = $presence_check->bindValue(":last_name", $last_name, PDO::PARAM_STR);
+  $presence_check->execute();
+
+  $present = $presence_check->fetchColumn() > 0;
+
+  if (!$present)
+  {
+    $check1 = $register_player->bindValue(":last_name", $last_name, PDO::PARAM_STR);
+    $check1 &= $register_player->bindValue(":score", $score, PDO::PARAM_INT); // Stylish way to check if previous binding failed or not
     
-  $ndf = $_POST["ndf"];
-  $score = (int)$_POST["score"];
+    /* TODO : add checks for both executes there */
+    $register_player->execute();
+
+    http_response_code(200);
+    echo json_encode(["success" => "Player successfully registered"]);
+  }
+  else
+  {
+    $check1 = $update_player->bindValue(":score", $score, PDO::PARAM_INT);
+    $check &= $update_player->bindValue(":last_name", $last_name, PDO::PARAM_STR);
+    
+    /* TODO : add checks for both executes there */
+    $update_player->execute();
+
+    http_response_code(200);
+    echo json_encode(["success" => "Player score successfully updated"]);
+  }
+
+
 ?>
