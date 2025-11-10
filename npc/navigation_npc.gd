411cd1b03dd1f81c363a3f4b768da3749cb7bcc3
Muhammed ERDAL <m.erdal677@proton.me>
2025-11-10 11:22:24 +0100

    Ajout de l'ajout dynamique des personnages non joueur aux scènes

--- npc/navigation_npc.gd
+++ npc/navigation_npc.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 class_name NavigationNPC 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
-@onready var model = $Nils
+@onready var model = $MeshInstance3D
 
 @export var npc_name: String = "NPC"
 @export var dialogue: String = "Bonjour !"
@@ -14,8 +14,22 @@ var stuck_timer: float = 0.0
 var STUCK_THRESHOLD: float = 0.0
 var SPEED: float = 2.0
 var Move: bool = true
+		
+func _init(name: String = "DefaultName", dialogue_text: String = "Bonjour !", model_name: String = "Nils") -> void:
+	self.npc_name = name
+	self.dialogue = dialogue_text
+	var path = "res://Import/Models/%s.fbx" % model_name
+	if ResourceLoader.exists(path):
+		var scene = load(path)
+		if scene is PackedScene:
+			model = scene.instantiate()
+			add_child(model)
+		else:
+			push_warning("Le fichier %s n'est pas une scène valide." % path)
+	else:
+		push_warning("Modèle %s introuvable dans Import/Models/" % model_name)
 
-
+	print("[DEBUG] : NPC créé : ", name, " - ", dialogue_text, " (modèle: ", model_name, ")")	
 
 
 func _physics_process(delta: float) -> void:
@@ -35,8 +49,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		stuck_timer = 0.0  # réinitialise si le NPC bouge
 		var target_ratotion = atan2(direction.x, direction.z)
-		var current_rotation = model.rotation.y
-		var new_rotation = lerp_angle(current_rotation, target_ratotion, 5.0 * delta)
+		var new_rotation = lerp_angle(model.rotation.y, target_ratotion, 5.0 * delta)
 		model.rotation = Vector3(0, new_rotation, 0)
 
 func _set_new_random_destination() -> void:
