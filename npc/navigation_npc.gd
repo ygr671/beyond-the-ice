f03856ede5fbfb04af43c75c1fe86c3f62565639
Muhammed ERDAL <m.erdal677@proton.me>
2025-11-10 12:56:50 +0100

    Ajout de NPC fixé, script reattaché

--- npc/navigation_npc.gd
+++ npc/navigation_npc.gd
@@ -11,10 +11,11 @@ func _display() -> String:
 	return "Je m'appelle " + npc_name + " " + dialogue
 
 var stuck_timer: float = 0.0
-var STUCK_THRESHOLD: float = 0.0
+var STUCK_THRESHOLD: float = 1.0
 var SPEED: float = 2.0
 var Move: bool = true
-		
+
+# Init
 func _init(name: String = "DefaultName", dialogue_text: String = "Bonjour !", model_name: String = "Nils") -> void:
 	self.npc_name = name
 	self.dialogue = dialogue_text
@@ -29,41 +30,53 @@ func _init(name: String = "DefaultName", dialogue_text: String = "Bonjour !", mo
 	else:
 		push_warning("Modèle %s introuvable dans Import/Models/" % model_name)
 
-	print("[DEBUG] : NPC créé : ", name, " - ", dialogue_text, " (modèle: ", model_name, ")")	
+	print("[DEBUG] : NPC créé :", name, "-", dialogue_text, "(modèle:", model_name, ")")
 
-
+# Mouvement
 func _physics_process(delta: float) -> void:
+	# Pour corriger le bug de "tremblement" et de bloquage soudain lors du déplacement du NPC
+
+	
+	if not Move:
+		return
+	
 	var destination = navigation_agent_3d.get_next_path_position()
 	var local_destination = destination - global_position
 	var direction = local_destination.normalized()
-	
+
 	velocity = direction * SPEED
 	move_and_slide()
-	
-	# Vérifie si le NPC est bloqué
+
+	# Rotation fluide uniquement quand il bouge
+	if velocity.length() > 0.05:
+		var target_rotation = atan2(direction.x, direction.z)
+		var new_rotation = lerp_angle(rotation.y, target_rotation, 5.0 * delta)
+		rotation.y = new_rotation
+
+	# Détection blocage
 	if velocity.length() < 0.01:
 		stuck_timer += delta
-		if stuck_timer >= STUCK_THRESHOLD and Move == true:
+		if stuck_timer >= STUCK_THRESHOLD:
 			_set_new_random_destination()
 			stuck_timer = 0.0
 	else:
-		stuck_timer = 0.0  # réinitialise si le NPC bouge
-		var target_ratotion = atan2(direction.x, direction.z)
-		var new_rotation = lerp_angle(model.rotation.y, target_ratotion, 5.0 * delta)
-		model.rotation = Vector3(0, new_rotation, 0)
+		stuck_timer = 0.0
 
+# Destinations
 func _set_new_random_destination() -> void:
 	Move = true
 	SPEED = 2.0
-	var random_position := Vector3.ZERO
-	random_position.x = randf_range(-5.0, 5.0)
-	random_position.z = randf_range(-5.0, 5.0)
+	var random_position := Vector3(
+		randf_range(-5.0, 5.0),
+		0,
+		randf_range(-5.0, 5.0)
+	)
 	navigation_agent_3d.set_target_position(random_position)
 
 func _set_destination_null() -> void:
 	SPEED = 0.0
-	Move  = false
+	Move = false
 
 func speed_boost() -> void:
 	SPEED = 2.0
-	Move  = true
+	Move = true
