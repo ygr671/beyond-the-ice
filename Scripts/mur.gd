df92f3845ee071632c6811c6fa1f1d2f838afa9f
julot <julot424@gmail.com>
2025-11-21 15:29:00 +0100

    Ajout des couleurs de murs

--- Scripts/mur.gd
+++ Scripts/mur.gd
@@ -1,8 +1,16 @@
 extends Node3D
-func set_color(new_color: Color, color_name: String):
-	if $MeshInstance3D.material_override == null:
-		$MeshInstance3D.material_override = StandardMaterial3D.new()
-	$MeshInstance3D.material_override.albedo_color = new_color
+#On charge chaque variente de couleur
+@onready var wall_mesh = $MeshInstance3D
+@onready var orange = preload("res://materials/Wall/Orange.tres")
+@onready var red = preload("res://materials/Wall/Red.tres")
+@onready var gray = preload("res://materials/Wall/Gray.tres")
+@onready var white = preload("res://materials/Wall/White.tres")
+@onready var black = preload("res://materials/Wall/Black.tres")
+@onready var green = preload("res://materials/Wall/Green.tres")
 
+
+
+func set_color(material_ressource: StandardMaterial3D, color_name: String):
+	wall_mesh.set_surface_override_material(0, material_ressource)
 	# émission du signal
 	player_controller.emit_signal("environment_changed", "color_changed", color_name)
