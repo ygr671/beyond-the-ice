1dceb1ee571d3e2a9bdd31a1a03e3ca7c7c71816
Muhammed ERDAL <m.erdal677@proton.me>
2025-11-26 14:04:27 +0100

    Test

--- Scripts/scene_manager.gd
+++ Scripts/scene_manager.gd
@@ -6,7 +6,7 @@ var scene_stack : Array = []
 # Aller vers une nouvelle scène
 func go_to_scene(new_scene_path: String) -> void:
 	# Si la pile est vide ou si on change de scène, stocke le chemin de la scène actuelle
-	# Attention : on **doit connaître le chemin actuel**, donc on le passe depuis le bouton
+	# Attention : on doit connaître le chemin actuel, donc on le passe depuis le bouton
 	scene_stack.append(new_scene_path) # on stocke le chemin qu'on quitte
 	get_tree().change_scene_to_file(new_scene_path)
 
