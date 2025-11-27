ec090906c54dc8931add2bc134be08ab48a50fc2
raphael.roche <raphael.roche@etu-unistra.iutrs.fr>
2025-11-27 19:47:15 +0100

    ajout d'un menu pour commander des meubles

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -2,5 +2,7 @@ extends Node
 var current_room: int  = 0
 
 signal environment_changed(change_type, data)
+signal furniture_ordered(furniture_type)
 func _ready():
 	emit_signal("environment_changed", "init", null)
+	emit_signal("furniture_ordered", "init")
