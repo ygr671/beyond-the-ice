9cede41e44e31a187c2092a9c528196196ed5b8a
raphael roche <raphael.roche@etu.unistra.fr>
2025-11-28 16:57:00 +0100

    ajout de la commande

---
+++ Scripts/charging_bar.gd
@@ -0,0 +1,58 @@
+extends Control
+@onready var fill_timer: Timer = $"Timer"
+@onready var progress_bar: ProgressBar = $"charging_bar_furniture"
+@onready var confirmation : Label = $"confirmation_message"
+
+
+var is_filling: bool = false
+
+
+func _ready():
+	player_controller.connect("furniture_ordered", Callable(self, "_on_furniture_ordered"))
+	randomize()
+	
+# --- Mise à jour de la barre et du Label à chaque frame ---
+func _process(delta: float):
+	if is_filling:
+		var temps_reel_ecoule = progress_bar.max_value - fill_timer.get_time_left()
+		progress_bar.value = temps_reel_ecoule
+		
+		
+func get_weighted_result(success_proba: float) -> int:
+	# Génère un nombre aléatoire entre 0.0 et 1.0
+	var random_value: float = randf()
+	if random_value < success_proba:
+		return 1 
+	else:
+		return 0
+		
+func _on_furniture_ordered(furniture_type: String):
+	start_filling_bar()
+	await get_tree().create_timer(15.0).timeout
+	
+	var rand = get_weighted_result(0.5)
+	_on_timer_timeout(rand)
+	if rand == 1:
+		match furniture_type:
+			"bed":
+				player_controller.bed_in_invetory +=1
+	
+	
+
+func start_filling_bar(): 
+	progress_bar.value = 0.0  
+	is_filling = true  
+	fill_timer.start()
+
+
+func _on_timer_timeout(random: int):
+	is_filling = false
+	progress_bar.value = progress_bar.max_value
+	progress_bar.hide()
+	progress_bar.value = progress_bar.min_value
+	if(random == 1):
+		confirmation.text = "Meuble livré avec succès !"
+	else:
+		confirmation.text = "Votre commande n'a pas pu etre livré !"
+	await get_tree().create_timer(2.0).timeout
+	confirmation.hide()
--- Scripts/ui_order_furniture.gd
+++ Scripts/ui_order_furniture.gd
@@ -1,18 +1,29 @@
 extends Control
 
 @onready var order_furniture = $"."
+@onready var charging_bar = $"../ui_charging_bar"
+@onready var item_list = $"../ui_order_furniture/Panel/ItemList"
+var furniture_order
+var is_processing: bool = false
 
 func _on_button_order_pressed() -> void:
 	order_furniture.show()
 
 
 func _on_item_list_item_selected(index: int) -> void:
-	order_furniture.hide()
-	await get_tree().create_timer(15.0).timeout
-	player_controller.bed_in_invetory +=1
-	if index == 0: 
-		emit_signal("furniture_ordered", "bed")
+	if is_processing:
+		return
+	is_processing = true
+	if index == 0:  
+		furniture_order = "bed"
 	elif index == 1:
-		emit_signal("furniture_ordered", "chair")
-	elif index == 2: 
-		emit_signal("furniture_ordered", "setup")
+		furniture_order = "chair"
+	elif index == 2:  
+		furniture_order = "setup"
+	is_processing = false
+	order_furniture.hide()
+	charging_bar.show()
+	item_list.deselect_all()
+	
+	player_controller.emit_signal("furniture_ordered", furniture_order)
+	
