eca48659b80e7e1fbfb43fb63045a613f6dac725
raphael roche <raphael.roche@etu.unistra.fr>
2025-11-28 14:45:51 +0100

    ajout d'un timer etc

--- Scripts/ui_order_furniture.gd
+++ Scripts/ui_order_furniture.gd
@@ -2,20 +2,17 @@ extends Control
 
 @onready var order_furniture = $"."
 
-
-
-
 func _on_button_order_pressed() -> void:
 	order_furniture.show()
 
 
 func _on_item_list_item_selected(index: int) -> void:
+	order_furniture.hide()
+	await get_tree().create_timer(15.0).timeout
+	player_controller.bed_in_invetory +=1
 	if index == 0: 
 		emit_signal("furniture_ordered", "bed")
-		order_furniture.hide()
 	elif index == 1:
 		emit_signal("furniture_ordered", "chair")
-		order_furniture.hide()
 	elif index == 2: 
 		emit_signal("furniture_ordered", "setup")
-		order_furniture.hide()
